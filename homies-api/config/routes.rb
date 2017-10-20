2c2fb489650da4ad91fbf25411dfff5911281fc0
Bryan Hunter <bdhunter3141@gmail.com>
2017-10-20 16:12:56 +0000

    Setup the routes.

--- homies-api/config/routes.rb
+++ homies-api/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show]
+    get '/article_likes', to: 'users#article_like'
+  resources :circles, only: [:create, :show, :update, :destroy] do
+    resources :circle_users, except: [:update]
+    resources :posts do
+      resources :post_comments
+    end
+  end
+  get 'articles/:article_id/likes', to: 'articles#like_number'
+  resources :article_comments
 end
