d95afe706f144c07ed052a32a124cb3494cfad4a
martincapo <martincapo@yahoo.com>
2017-10-22 01:01:51 +0000

    minor bug fix

--- homies-react/src/NavBar.js
+++ homies-react/src/NavBar.js
@@ -20,7 +20,7 @@ class NavBar extends Component {
         <Navbar inverse collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Homies</a>
+              <a href="/">Homies</a>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
