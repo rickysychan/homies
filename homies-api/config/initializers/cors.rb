224759443ab34791bd8a42c968aa53886af93103
martincapo <martincapo@yahoo.com>
2017-10-24 15:20:44 +0000

    minor fix

--- homies-api/config/initializers/cors.rb
+++ homies-api/config/initializers/cors.rb
@@ -5,17 +5,6 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-<<<<<<< HEAD
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'localhost'
-
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
-=======
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000'
@@ -25,4 +14,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
->>>>>>> react-rebuild
