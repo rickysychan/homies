94dfe539d2f4613277f9f46ec4ae7613f928e05b
Bryan Hunter <bdhunter3141@gmail.com>
2017-10-25 16:58:59 +0000

    Got custom Watson Model working.

--- homies-api/app/controllers/api/v1/recommendations_controller.rb
+++ homies-api/app/controllers/api/v1/recommendations_controller.rb
@@ -23,16 +23,28 @@ module Api::V1
     # Index
 
     def index
-      @recommendations = watson
+      @recommendations = watson("entities")
       render json: @recommendations
     end
 
     private
 
-    def top_three
-      @@client.search("ghostbusters love", result_type: "recent").take(3).each do |tweet|
+    def tweet_search(movie_name)
+      @@client.search(movie_name, result_type: "recent").take(100).each do |tweet|
         tweet.text
       end
+    end
+
+    def list_of_tweets_text(tweets)
+      @tweet_text = []
+      tweets.each do |tweet|
+        @tweet_text.push(tweet.text)
+      end
+      @tweet_text
+    end
+
+    def user_tweets(user_id)
+      @@client.user_timeline(user_id, options = { :count => 200 })
     end
 
     def fringe
@@ -43,21 +55,27 @@ module Api::V1
       GiantBomb::Search.new().query('Need for Speed').resources('game').fetch
     end
 
-    def watson
+    def watson(features)
+      # @text_array = []
+      # array.each do |text|
        response = Excon.get("https://watson-api-explorer.mybluemix.net/natural-language-understanding/api/v1/analyze",
          :headers => { "Accept" => "application/json" },
          :query => { "version"        => "2017-02-27",
-                     "text"           => "My next task was to take the JSON object this returned and format it into a hash with just the information I needed so I could use it to instantiate an object from another class using mass assignment. To do this I used the OpenStruct gem which made it easier to iterate through the data and pull out only what I needed. Plus, I had worked with hashes before but I had never used Ostruct, so it was another learning opportunity.",
-                     "features"       => "categories",
+                     "url"           => "https://twitter.com/ign",
+                     "features"       => features,
                      "language"       => "en",
                      "concepts.limit" => 8,
                      "entities.limit" => 50,
-                     "keywords.limit" => 50
+                     "entities.model" => "10:949c5dac-6522-4da6-9526-890c5983ff26"
                     },
          :user => ENV["WATSON_USERNAME"],
          :password => ENV["WATSON_PASSWORD"],
        )
+
        response.body
+     #   @text_array.push(response.body.force_encoding(Encoding::UTF_8))
+     # end
+     # @text_array
     end
 
   end
