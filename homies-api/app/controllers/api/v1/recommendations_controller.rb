10f617ee2512ae5cd53fdfac09a8f22b21b524bd
Bryan Hunter <bdhunter3141@gmail.com>
2017-10-27 16:47:46 +0000

    Watson will now return a list of entertainment products with the number of mentions next to it.

--- homies-api/app/controllers/api/v1/recommendations_controller.rb
+++ homies-api/app/controllers/api/v1/recommendations_controller.rb
@@ -23,7 +23,7 @@ module Api::V1
     # Index
 
     def index
-      @recommendations = twitter_user_products(watson("ign", "entities"))
+      @recommendations = sort_results_by_number(submit_to_watson)
       render json: @recommendations
     end
 
@@ -33,45 +33,105 @@ module Api::V1
 
     private
 
-    def tweet_search(movie_name)
-      @@client.search(movie_name, result_type: "recent").take(100).each do |tweet|
-        tweet.text
+    def tweet_search(product)
+      @users = []
+      results = @@client.search(product, result_type: "recent").take(20)
+      results.each do |tweet|
+        @users.push(tweet["user"]["id"])
       end
+      @users
     end
 
-    def list_of_tweets_text(tweets)
-      @tweet_text = []
-      tweets.each do |tweet|
-        @tweet_text.push(tweet.text)
+    def similar_users(product_array)
+      @similar_users = []
+      product_array.each do |product|
+        users = tweet_search(product)
+        users.each { |user| @similar_users.push(user) }
       end
-      @tweet_text
+      @similar_users
     end
 
-    def user_tweets(user_id)
-      @@client.user_timeline(user_id, options = { :count => 200 })
+    def add_users_to_list(similar_users_array)
+      list = @@client.create_list("Homies", options = { mode: 'private'})
+      @list_id = list["id"]
+      @@client.add_list_members(@list_id, similar_users_array)
+      @list_id
     end
 
-    def fringe
-      Tmdb::TV.find("fringe")
+
+    def similar_list_tweets(list_id)
+      @similar_users_tweets = []
+      @@client.list_timeline(list_id, options = { :count => 50 }).each do |tweet|
+        @similar_users_tweets.push(tweet["text"])
+      end
+      @similar_users_tweets
     end
 
-    def need_for_speed
-      GiantBomb::Search.new().query('Need for Speed').resources('game').fetch
+
+    def search_all(query)
+      @results = []
+      @results.push(tv_search(query))
+      @results.push(movie_search(query))
+      @results.push(game_search(query))
+      @results
+    end
+
+    def tv_search(show)
+      Tmdb::TV.find(show)
+    end
+
+    def movie_search(movie)
+      Tmdb::Movie.find(movie)
+    end
+
+    def game_search(game)
+      GiantBomb::Search.new().query(game).resources('game').fetch
     end
 
     def twitter_user_products(body)
+      @twitter_product_list = []
       parsed_body = JSON.parse(body)
       parsed_body["entities"].each do |product|
-        puts product["text"]
+        @twitter_product_list.push(product["text"])
       end
+      @twitter_product_list
     end
 
-    def watson(twitter_user, features)
+    def return_similar_user_texts(products_array)
+      @tweet_list = []
+      while @tweet_list.empty? do
+        sleep(5)
+        @tweet_list = similar_list_tweets(add_users_to_list(similar_users(products_array)))
+      end
+      @tweet_list
+    end
+
+    def submit_to_watson
+      @results = {}
+      @text = return_similar_user_texts(["Big Fish", "Call of Duty", "Bob's Burgers", "Dante's Peak", "Ninja Turtles"]).to_s
+      while @results.empty? do
+        sleep(10)
+        @results = watson(@text)
+      end
+      parsed_results = JSON.parse(@results)
+      entities = parsed_results["entities"]
+      entities.map! do |entity|
+        entity["text"]
+      end
+      entities
+    end
+
+    def sort_results_by_number(array)
+      results_hash = array.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
+      results_hash.sort_by(&:last).to_h
+    end
+
+    def watson(text)
        response = Excon.get("https://watson-api-explorer.mybluemix.net/natural-language-understanding/api/v1/analyze",
          :headers => { "Accept" => "application/json" },
          :query => { "version"        => "2017-02-27",
-                     "url"           => "https://twitter.com/#{twitter_user}",
-                     "features"       => features,
+                     "text"           => text,
+                     "features"       => "entities",
                      "language"       => "en",
                      "concepts.limit" => 8,
                      "entities.limit" => 50,
