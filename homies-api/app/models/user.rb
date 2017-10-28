ab2bcd4ae09bfa4dcaa3f8472ac0ce90efb28d15
martincapo <martincapo@yahoo.com>
2017-10-28 16:17:42 +0000

    minor fix

--- homies-api/app/models/user.rb
+++ homies-api/app/models/user.rb
@@ -13,5 +13,4 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password_digest, length: { minimum: 4 }
 end
