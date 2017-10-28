83b07665462bfdc57ab0d49c62d36a8275bdc333
Ricky Chan <rickysychan@gmail.com>
2017-10-28 16:08:36 +0000

    removed google api

--- homies-react/src/routes.jsx
+++ homies-react/src/routes.jsx
@@ -9,7 +9,6 @@ import Login from './components/Login.jsx';
 import Register from './components/Register.jsx';
 import LoginScreen from './components/LoginScreen.jsx';
 // TODO : delete once we finish the developement
-import GoogleAPI from './components/GoogleAPI.jsx';
 import NewCircleForm from './components/NewCircleForm.jsx';
 
 export default (
@@ -21,7 +20,6 @@ export default (
     <Route path="/register" component={Register} />
     <Route path="/loginscreen" component={LoginScreen} />
     <Route path="/circles" component={Circle} />
-    <Route path="/googleapi" component={GoogleAPI} />
     <Route path="/NewCircle" component={NewCircleForm} />
   </Route>
 );
